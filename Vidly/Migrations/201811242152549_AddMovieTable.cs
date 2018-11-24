1132e1733d223dadfae84d0fc6e19b7369ceb096
Seetam Pradhan <seetampradhan@live.com>
2018-11-25 03:24:07 +0530

    Upadted Movie Entity.

---
+++ Vidly/Migrations/201811242152549_AddMovieTable.cs
@@ -0,0 +1,34 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        GenreId = c.Int(nullable: false),
+                        ReleaseDate = c.DateTime(nullable: false),
+                        MovieAddedDate = c.DateTime(nullable: false),
+                        MovieStock = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
+                .Index(t => t.GenreId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropTable("dbo.Movies");
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,5 +9,10 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime MovieAddedDate { get; set; }
+        public int MovieStock { get; set; }
     }
 }
