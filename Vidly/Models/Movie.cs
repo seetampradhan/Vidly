152ba5e93c8b93ab96821ae202c1be42c19f9ed8
Seetam Pradhan <seetampradhan@live.com>
2018-11-25 03:35:29 +0530

    Added DataAnnotations To Movie  entity.

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Movie
     {
+        
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }
         public int GenreId { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime MovieAddedDate { get; set; }
+
+        [Required]
         public int MovieStock { get; set; }
     }
 }
