3da7310d57a3e6492043acda8a59fce02416d4da
Seetam Pradhan <seetampradhan@live.com>
2018-11-25 02:24:16 +0530

    Added Name Column to MembershipType Table.

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class MembershipType
     {
         public int Id { get; set; }
+
+        [Required,StringLength(255)]
+        public string Name { get; set; }
+
         public short SignupFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
